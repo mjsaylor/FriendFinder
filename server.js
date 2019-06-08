fb916c4254da73440865a9204ed199cb3ccfe2d0
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-06-07 21:02:17 -0500

    set up ports and routes in server.js

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 // Your server.js file should require the basic npm packages we've used in class: express and path.
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
 
