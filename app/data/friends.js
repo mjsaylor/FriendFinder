9249c3274afbe9889fec4a8613a9cf32c41f4cfe
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-06-07 20:23:19 -0500

    added data sample and routes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+const friendsArray = [
+    {
+        name: "Maggie Jo",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjdhPHe1tjiAhVwT98KHdQrBysQjRx6BAgBEAU&url=https%3A%2F%2Fsingandstomp.com%2F2019%2F02%2F01%2Fmidwest-sing-stomp-presents-square-dance-cajun-fais-do-do-3-24-19%2F&psig=AOvVaw2Se0RTgmoDFZdb8CMifafh&ust=1560041720674361",
+        scores: [5, 3, 4, 5, 2, 2, 4, 3, 3, 5]
+    }
+]
+
+module.exports = friendsArray;
