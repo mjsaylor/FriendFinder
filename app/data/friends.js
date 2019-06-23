31eceab3d504946b2f8b600d42aa9634f458df63
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-06-23 15:52:59 -0500

    added correct picture links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 const friendsArray = [
     {
         name: "Maggie Jo",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjdhPHe1tjiAhVwT98KHdQrBysQjRx6BAgBEAU&url=https%3A%2F%2Fsingandstomp.com%2F2019%2F02%2F01%2Fmidwest-sing-stomp-presents-square-dance-cajun-fais-do-do-3-24-19%2F&psig=AOvVaw2Se0RTgmoDFZdb8CMifafh&ust=1560041720674361",
+        photo: "https://www.google.com/url?sa=i&source=imahttps://singandstomp.files.wordpress.com/2019/02/maggiejo.jpg",
         scores: [5, 3, 4, 5, 2, 2, 4, 3, 3, 5]
     }, 
     {
         name: "Derek",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiBobK---fiAhUIheAKHe7RCpMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.ioimprov.com%2Fperformers%2Fderek-shoemaker%2F&psig=AOvVaw1zLHbmGeTBR5AVWDybRVlD&ust=1560567062901883https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiBobK---fiAhUIheAKHe7RCpMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.ioimprov.com%2Fperformers%2Fderek-shoemaker%2F&psig=AOvVaw1zLHbmGeTBR5AVWDybRVlD&ust=1560567062901883",
+        photo: "https://www.ioimprov.com/files/2017/04/derek-shoemaker-1.jpg",
         scores: [5, 2, 5, 5, 3, 5, 4, 2, 3, 3]
     },
     {
@@ -16,7 +16,7 @@ const friendsArray = [
     },
     {
         name: "Matt",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjbmZOc_OfiAhVDR60KHQDlCnIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.ioimprov.com%2Fperformers%2Fmatt-myers%2F&psig=AOvVaw0ETa-YqFXGA6GHQeCh_cRb&ust=1560567259828524",
+        photo: "https://www.ioimprov.com/files/2017/04/matt-myers.jpg",
         scores: [2, 2, 2, 3, 5, 4, 1, 4, 4, 5]
     }
 ]
